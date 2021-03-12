0111e88c1946c21c4facd0f115ef823ce7cc3126
Yevhenii Korkots <atc0884@gmail.com>
2021-03-12 23:26:53 +0200

    preparetion

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-function App() {
-  return <div className="App"></div>;
+class App extends Component {
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
